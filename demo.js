e6e7404748717577d986d2b34b039b843088c674
Sandesh Eduru <96225748+sandesheduru@users.noreply.github.com>
2021-12-17 15:20:48 +0530

    Update demo.js
    Color change

--- demo.js
+++ demo.js
@@ -2,7 +2,7 @@
 <html>
 <body>
 <p>I am normal</p>
-<p style="color:red;">I am red</p>
+<p style="color:red;">I am blue</p>
 <p style="color:blue;">I am Green</p>
 <p style="font-size:50px;">I am Small</p>
 </body>
