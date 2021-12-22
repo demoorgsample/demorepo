b3b2cbb85ef54227be046b92da2a28622b3eef25
Sandesh Eduru <96225748+sandesheduru@users.noreply.github.com>
2021-12-22 19:50:38 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -3,7 +3,7 @@
 <body>
 <p>I am normal</p>
 <p style="color:red;">I am red</p>
-<p style="color:blue;">I am Green</p>
+<p style="color:blue;">I am Blue</p>
 <p style="font-size:50px;">I am Small</p>
 </body>
 </html>
